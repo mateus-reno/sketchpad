4def6ec5134e958e3b1cb7ee2612a6de349bda49
Mateus Kratz <mateuskratz@gmail.com>
2017-10-13 14:48:35 +0300

    Added 'clear' button and 'New Grid' button, styled css

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
   var columnWidth = 16;
   var columnHeight = 16;
 
+  function randomColorChange(){
+    return '#'+(Math.floor(Math.random()*16777216)&0xFFFFFF).toString(16);
+  }
+
   function makeBoxes() {
     //boxcount lets us set how many times we want the for loop to run...when we change the columns/rows later this variable will be updated
     var boxCount = columnWidth * columnHeight;
@@ -14,16 +18,19 @@ $(document).ready(function() {
     //we only want to declare this once so we place it after the loop
     $(".squares").width((500 / columnWidth) - 2);
     $(".squares").height((500 / columnHeight) - 2);
-    $('.squares').hover(
-      function() {
-        $(this).addClass('hover');
+    $('.squares').hover(function() {
+        $(this).css('background-color', randomColorChange());
       }
     );
   }
   //fire the initial function
   makeBoxes();
 
-  $('button').on('click', function() {
+  $('#clear').on('click', function() {
+    $('.squares').css('background-color', '#fff');
+  });
+
+  $('#start').on('click', function() {
     $('div').remove('.squares');
     var columnCount = prompt("Please enter the number of squares for the height of the grid")
     var rowCount = prompt("Please enter the number of squares for the width of the grid")
