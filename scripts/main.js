b45cdfb781a9dd88eed97f291cab03bff82023c4
Mateus Kratz <mateuskratz@gmail.com>
2017-10-13 11:18:37 +0300

    Added grid and hover on main.js; styled grid on stylesheet.css

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  //declare the variables at the top of  your functions...it enables us to change them later
+  var columnWidth = 16;
+  var columnHeight = 16;
+
+  function makeBoxes() {
+    //boxcount lets us set how many times we want the for loop to run...when we change the columns/rows later this variable will be updated
+    var boxCount = columnWidth * columnHeight;
+  //
+    for (var i = 0; i < boxCount; i++) { //loop through each box
+    //any code you place in here will execute each time we loop around
+      $("<div class='squares'></div>").appendTo('#main');
+    }
+    //we only want to declare this once so we place it after the loop
+    $(".squares").width((780 / columnWidth) - 2);
+    $(".squares").height((780 / columnHeight) - 2);
+    $('.squares').hover(
+      function() {
+        $(this).addClass('hover');
+      }
+    );
+  }
+  //fire the initial function
+  makeBoxes();
+});
